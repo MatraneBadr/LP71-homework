1a436ab0ff205c0669289763591e97225d3c0491
unknown <matrane.badr@gmail.com>
2020-03-10 13:28:41 +0100

    add Nutriments

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,2 +1,6 @@
 class Burger < ApplicationRecord
+    def nutriments
+        product = Openfoodfacts::Product.get(self.code, locale: 'fr');
+        return product == nil ? [["No Data"],[""]] : product.nutriments
+    end
 end
